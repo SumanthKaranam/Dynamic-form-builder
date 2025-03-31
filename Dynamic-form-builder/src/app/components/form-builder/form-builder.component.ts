d61fe8ac97362d29a940ef18f4914b10e0f65af4
SumanthKaranam <98143771+SumanthKaranam@users.noreply.github.com>
2025-03-31 11:30:15 +0530

    Update form-builder.component.ts

--- Dynamic-form-builder/src/app/components/form-builder/form-builder.component.ts
+++ Dynamic-form-builder/src/app/components/form-builder/form-builder.component.ts
@@ -53,72 +53,11 @@ export class FormBuilderComponent {
     this.formGroup = this.fb.group({});
   }
 
-  // onFieldDrop(field: FormField) {
-  //   // Remove the field from availableFields
-  //   const index = this.availableFields.indexOf(field);
-  //   if (index > -1) {
-  //     this.availableFields.splice(index, 1);
-  //   }
-
-  //   // Ensure the field has a unique name
-  //   const newField = { ...field, name: `${field.name}_${this.formFields.length}` };
-  //   this.formFields.push(newField);
-  //   this.formGroup.addControl(newField.name, new FormControl(''));
-  // }
-
   onFieldDrop(field: FormField) {
     // Create a copy of the field to add to formFields
     const newField = { ...field, name: `${field.name}_${this.formFields.length}` };
     this.formFields.push(newField);
     this.formGroup.addControl(newField.name, new FormControl(''));
-  }
-
-  // removeField(index: number) {
-  //   // Remove the field from formFields
-  //   const field = this.formFields[index];
-  //   this.formFields.splice(index, 1);
-  //   this.formGroup.removeControl(field.name);
-
-  //   // Add the field back to availableFields with its original properties, including the 'name'
-  //   const { name, ...rest } = field; // Keep the name property (with unique suffix) for re-adding
-  //   this.availableFields.push({
-  //     name, ...rest,
-  //     id: '',
-  //     required: false,
-  //     type: rest.type as 'text' | 'textarea' | 'dropdown' | 'checkbox' | 'radio' | 'date'
-  //   });
-  // }
-
-  // onDrop(event: CdkDragDrop<FormField[]>) {
-  //   if (event.previousContainer === event.container) {
-  //     // Reorder within the same container
-  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     // Transfer between containers
-  //     const draggedField = event.previousContainer.data[event.previousIndex];
-
-  //     if (event.container.id === 'availableFields') {
-  //       // Dragging from formFields to availableFields (edit/remove field)
-  //       this.removeField(event.previousIndex);
-  //     } else if (event.container.id === 'formFields') {
-  //       // Dragging from availableFields to formFields (add field)
-  //       this.onFieldDrop(draggedField);
-  //     }
-
-  //     transferArrayItem(
-  //       event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex
-  //     );
-  //   }
-  // }
-
-  removeField(index: number) {
-    // Remove the field from formFields
-    const field = this.formFields[index];
-    this.formFields.splice(index, 1);
-    this.formGroup.removeControl(field.name);
   }
 
   onDrop(event: CdkDragDrop<FormField[]>) {
@@ -146,7 +85,7 @@ export class FormBuilderComponent {
     }
   }
 
-  createTemplate() {
+    createTemplate() {
     let template = '<form>';
     this.formFields.forEach((field) => {
       if (field.type === 'text') {
@@ -164,11 +103,41 @@ export class FormBuilderComponent {
       }
     });
     template += '</form>';
-
+  
     const sanitizedTemplate = this.sanitizer.bypassSecurityTrustHtml(template);
     this.templates.push({ formGroup: this.fb.group({}), formFields: [...this.formFields], preview: sanitizedTemplate });
+  
+    // Reset formFields and formGroup
     this.formFields = [];
     this.formGroup = this.fb.group({});
+  
+    // Reset availableFields to its original state
+    this.availableFields = [
+      { type: 'text', label: 'Text Input', name: 'textInput', id: '', required: false, helpText: 'Enter text here' },
+      { type: 'textarea', label: 'Text Area', name: 'textArea', id: '', required: false, helpText: 'Enter long text here' },
+      { type: 'dropdown', label: 'Dropdown', name: 'dropdown', options: ['Option 1', 'Option 2'], id: '', required: false },
+      { type: 'checkbox', label: 'Checkbox', name: 'checkbox', id: '', required: false },
+      { type: 'radio', label: 'Radio Buttons', name: 'radio', options: ['Option A', 'Option B'], id: '', required: false },
+      { type: 'date', label: 'Date Picker', name: 'datePicker', id: '', required: false }
+    ];
+  }
+  
+  removeField(index: number) {
+    // Remove the field from formFields
+    const field = this.formFields[index];
+    this.formFields.splice(index, 1);
+    this.formGroup.removeControl(field.name);
+  
+    // Add the field back to availableFields
+    const originalField = this.availableFields.find(f => f.name.split('_')[0] === field.name.split('_')[0]);
+    if (!originalField) {
+      this.availableFields.push({
+        ...field,
+        name: field.name.split('_')[0], // Reset the name to its original value
+        id: '',
+        required: false
+      });
+    }
   }
 
   addTemplate() {
